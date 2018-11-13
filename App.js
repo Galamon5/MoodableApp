971700cadf5f1ff796f7ff703fbcda04e0c57b1f
Galamon Mac <galamon5@hotmail.com>
2018-11-12 18:51:39 -0600

    Se descompuso

--- App.js
+++ App.js
@@ -1,424 +1,121 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import {
-  Platform,
-  ScrollView,
+  AppRegistry,
   StyleSheet,
-  Switch,
   Text,
-  TouchableOpacity,
-  TouchableHighlight,
-  View,
-  Modal,
-  ActivityIndicator,
-  Image
-} from 'react-native'
+  SafeAreaView,
+  FlatList,
+  Platform,
+  Button
+} from "react-native";
+//import { BleManager, Device, BleError, LogLevel } from "react-native-ble-plx";
 
-import Toast from '@remobile/react-native-toast'
-import BluetoothSerial from 'react-native-bluetooth-serial'
-import { Buffer } from 'buffer'
-global.Buffer = Buffer
-const iconv = require('iconv-lite')
+type Props = {};
 
-const Button = ({ title, onPress, style, textStyle }) =>
-  <TouchableOpacity style={[ styles.button, style ]} onPress={onPress}>
-    <Text style={[ styles.buttonText, textStyle ]}>{title.toUpperCase()}</Text>
-  </TouchableOpacity>
+type State = {
+  text: Array<string>
+};
 
+function arrayBufferToHex(buffer) {
+  if (!buffer) return null;
+  const values = new Uint8Array(buffer);
+  var string = "";
+  for (var i = 0; i < values.length; i += 1) {
+    const num = values[i].toString(16);
+    string += num.length == 1 ? "0" + num : num;
+  }
+  return string;
+}
 
-const DeviceList = ({ devices, connectedId, showConnectedIcon, onDevicePress }) =>
-  <ScrollView style={styles.container}>
-    <View style={styles.listContainer}>
-      {devices.map((device, i) => {
-        return (
-          <TouchableHighlight
-            underlayColor='#DDDDDD'
-            key={`${device.id}_${i}`}
-            style={styles.listItem} onPress={() => onDevicePress(device)}>
-            <View style={{ flexDirection: 'row' }}>
-              {showConnectedIcon
-              ? (
-                <View style={{ width: 48, height: 48, opacity: 0.4 }}>
-                  {connectedId === device.id
-                  ? (
-                    <Image style={{ resizeMode: 'contain', width: 24, height: 24, flex: 1 }} source={require('./images/ic_done_black_24dp.png')} />
-                  ) : null}
-                </View>
-              ) : null}
-              <View style={{ justifyContent: 'space-between', flexDirection: 'row', alignItems: 'center' }}>
-                <Text style={{ fontWeight: 'bold' }}>{device.name}</Text>
-                <Text>{`<${device.id}>`}</Text>
-              </View>
-            </View>
-          </TouchableHighlight>
-        )
-      })}
-    </View>
-  </ScrollView>
-
-class MoodBluetooth extends Component {
-  constructor (props) {
-    super(props)
+export default class App extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
     this.state = {
-      isEnabled: false,
-      discovering: false,
-      devices: [],
-      unpairedDevices: [],
-      connected: false,
-      section: 0
-    }
+      text: ['HOla','AMigos']
+    };
   }
+  //
+  // componentDidMount() {
+  //   const manager = new BleManager();
+  //   manager.onStateChange(newState => {
+  //     if (newState != "PoweredOn") return;
+  //     this._log("Started scanning...");
+  //     manager.startDeviceScan(
+  //       null,
+  //       {
+  //         allowDuplicates: true
+  //       },
+  //       (error, device) => {
+  //         if (error) {
+  //           this._logError("SCAN", error);
+  //           return;
+  //         }
+  //         this._log("Device: " + device.name, device);
+  //       }
+  //     );
+  //   }, true);
+  // }
+  //
+  // _log = (text: string, ...args) => {
+  //   const message = "[" + Date.now() % 10000 + "] " + text;
+  //   this.setState({
+  //     text: [message, ...this.state.text]
+  //   });
+  // };
+  //
+  // _logError = (tag: string, error: BleError) => {
+  //   this._log(
+  //     tag +
+  //       "ERROR(" +
+  //       error.errorCode +
+  //       "): " +
+  //       error.message +
+  //       "\nREASON: " +
+  //       error.reason +
+  //       " (att: " +
+  //       error.attErrorCode +
+  //       ", ios: " +
+  //       error.iosErrorCode +
+  //       ", and: " +
+  //       error.androidErrorCode +
+  //       ")"
+  //   );
+  // };
+  //
+  // delay = () => {
+  //   return new Promise((resolve, reject) => {
+  //     setTimeout(() => {
+  //       resolve();
+  //     }, 5000);
+  //   });
+  // };
 
-  componentWillMount () {
-    Promise.all([
-      BluetoothSerial.isEnabled(),
-      BluetoothSerial.list()
-    ])
-    .then((values) => {
-      alert(values);
-      const [ isEnabled, devices ] = values
-      this.setState({ isEnabled, devices })
-      alert(devices[0])
-    })
-
-    BluetoothSerial.on('bluetoothEnabled', () => {
-      Toast.showShortBottom('Bluetooth Activado');
-      this.setState({ isEnabled: true });
-
-      })
-    BluetoothSerial.on('bluetoothDisabled', () => {
-      Toast.showShortBottom('Bluetooth desactivado')
-      this.setState({ isEnabled: false });
-    })
-    BluetoothSerial.on('error', (err) => console.log(`Error: ${err.message}`))
-    BluetoothSerial.on('connectionLost', () => {
-      if (this.state.device) {
-        Toast.showShortBottom(`Connection to device ${this.state.device.name} has been lost`)
-      }
-      this.setState({ connected: false })
-    })
-  }
-
-  /**
-   * [android]
-   * request enable of bluetooth from user
-   */
-  requestEnable () {
-    Toast.showShortBottom('Porfavor activa tu bluetooth');
-  }
-
-  /**
-   * [android]
-   * enable bluetooth on device
-   */
-  enable () {
-    this.setState({ isEnabled: true })
-  }
-
-  /**
-   * [android]
-   * disable bluetooth on device
-   */
-  disable () {
-    //BluetoothSerial.disable()
-    this.setState({ isEnabled: false })
-  }
-
-  /**
-   * [android]
-   * toggle bluetooth
-   */
-  toggleBluetooth (value) {
-    if (value === true) {
-      this.enable()
-    } else {
-      this.disable()
-    }
-  }
-
-  /**
-   * [android]
-   * Discover unpaired devices, works only in android
-   */
-  discoverUnpaired () {
-    if (this.state.discovering) {
-      return false
-    } else {
-      this.setState({ discovering: true })
-      BluetoothSerial.discoverUnpairedDevices()
-      .then((unpairedDevices) => {
-        this.setState({ unpairedDevices, discovering: false })
-      })
-      .catch((err) => Toast.showShortBottom(err.message))
-    }
-  }
-
-  /**
-   * [android]
-   * Discover unpaired devices, works only in android
-   */
-  cancelDiscovery () {
-    if (this.state.discovering) {
-      BluetoothSerial.cancelDiscovery()
-      .then(() => {
-        this.setState({ discovering: false })
-      })
-      .catch((err) => Toast.showShortBottom(err.message))
-    }
-  }
-
-  /**
-   * [android]
-   * Pair device
-   */
-  pairDevice (device) {
-    BluetoothSerial.pairDevice(device.id)
-    .then((paired) => {
-      if (paired) {
-        Toast.showShortBottom(`Device ${device.name} paired successfully`)
-        const devices = this.state.devices
-        devices.push(device)
-        this.setState({ devices, unpairedDevices: this.state.unpairedDevices.filter((d) => d.id !== device.id) })
-      } else {
-        Toast.showShortBottom(`Device ${device.name} pairing failed`)
-      }
-    })
-    .catch((err) => Toast.showShortBottom(err.message))
-  }
-
-  /**
-   * Connect to bluetooth device by id
-   * @param  {Object} device
-   */
-  connect (device) {
-    this.setState({ connecting: true })
-    BluetoothSerial.connect(device.id)
-    .then((res) => {
-      Toast.showShortBottom(`Connected to device ${device.name}`)
-      this.setState({ device, connected: true, connecting: false })
-    })
-    .catch((err) => Toast.showShortBottom(err.message))
-  }
-
-  /**
-   * Disconnect from bluetooth device
-   */
-  disconnect () {
-    BluetoothSerial.disconnect()
-    .then(() => this.setState({ connected: false }))
-    .catch((err) => Toast.showShortBottom(err.message))
-  }
-
-  /**
-   * Toggle connection when we have active device
-   * @param  {Boolean} value
-   */
-  toggleConnect (value) {
-    if (value === true && this.state.device) {
-      this.connect(this.state.device)
-    } else {
-      this.disconnect()
-    }
-  }
-
-  /**
-   * Write message to device
-   * @param  {String} message
-   */
-  write (message) {
-    if (!this.state.connected) {
-      Toast.showShortBottom('You must connect to device first')
-    }
-
-    BluetoothSerial.write(message)
-    .then((res) => {
-      Toast.showShortBottom('Successfuly wrote to device')
-      this.setState({ connected: true })
-    })
-    .catch((err) => Toast.showShortBottom(err.message))
-  }
-
-  onDevicePress (device) {
-    if (this.state.section === 0) {
-      this.connect(device)
-    } else {
-      this.pairDevice(device)
-    }
-  }
-
-  writePackets (message, packetSize = 64) {
-    const toWrite = iconv.encode(message, 'cp852')
-    const writePromises = []
-    const packetCount = Math.ceil(toWrite.length / packetSize)
-
-    for (var i = 0; i < packetCount; i++) {
-      const packet = new Buffer(packetSize)
-      packet.fill(' ')
-      toWrite.copy(packet, 0, i * packetSize, (i + 1) * packetSize)
-      writePromises.push(BluetoothSerial.write(packet))
-    }
-
-    Promise.all(writePromises)
-    .then((result) => {
-    })
-  }
-
-  render () {
-    const activeTabStyle = { borderBottomWidth: 6, borderColor: '#009688' }
+  render() {
     return (
-      <View style={{ flex: 1 }}>
-        <View style={styles.topBar}>
-          <Text style={styles.heading}>Moodable</Text>
-          {Platform.OS === 'ios'
-          ? (
-            <View style={styles.enableInfoWrapper}>
-              <Text style={{ fontSize: 12, color: '#FFFFFF' }}>
-                {this.state.isEnabled ? 'Desactivado' : 'Activado'}
-              </Text>
-              <Switch
-                onValueChange={this.toggleBluetooth.bind(this)}
-                value={this.state.isEnabled}
-                disabled={true}/>
-            </View>
-          ) : null}
-        </View>
-
-        {Platform.OS === 'ios'
-        ? (
-          <View style={[styles.topBar, { justifyContent: 'center', paddingHorizontal: 0 }]}>
-            <TouchableOpacity style={[styles.tab, this.state.section === 0 && activeTabStyle]} onPress={() => this.setState({ section: 0 })}>
-              <Text style={{ fontSize: 14, color: '#FFFFFF' }}>Dispositivos Mood</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={[styles.tab, this.state.section === 1 && activeTabStyle]} onPress={() => this.setState({ section: 1 })}>
-              <Text style={{ fontSize: 14, color: '#FFFFFF' }}>Dispositivos no apareados</Text>
-            </TouchableOpacity>
-          </View>
-        ) : null}
-        {this.state.discovering && this.state.section === 1
-        ? (
-          <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <ActivityIndicator
-              style={{ marginBottom: 15 }}
-              size={60} />
-            <Button
-              textStyle={{ color: '#FFFFFF' }}
-              style={styles.buttonRaised}
-              title='Cancel Discovery'
-              onPress={() => this.cancelDiscovery()} />
-          </View>
-        ) : (
-          <DeviceList
-            showConnectedIcon={this.state.section === 0}
-            connectedId={this.state.device && this.state.device.id}
-            devices={this.state.section === 0 ? this.state.devices : this.state.unpairedDevices}
-            onDevicePress={(device) => this.onDevicePress(device)} />
-        )}
-
-
-        <View style={{ alignSelf: 'flex-end', height: 52 }}>
-          <ScrollView
-            horizontal
-            contentContainerStyle={styles.fixedFooter}>
-            {Platform.OS === 'ios' && this.state.section === 1
-            ? (
-              <Button
-                title={this.state.discovering ? '... Discovering' : 'Discover devices'}
-                onPress={this.discoverUnpaired.bind(this)} />
-            ) : null}
-            {Platform.OS === 'ios' && !this.state.isEnabled
-            ? (
-              <Button
-                title='Request enable'
-                onPress={() => this.requestEnable()} />
-            ) : null}
-          </ScrollView>
-        </View>
-      </View>
-    )
+      <SafeAreaView
+        style={styles.container}
+      >
+        <Button
+          onPress={() => {
+            this.setState({
+              text: []
+            });
+          }}
+          title={"Clear"}
+        />
+        <FlatList
+          style={styles.container}
+          data={this.state.text}
+          renderItem={({ item }) => <Text> {item} </Text>}
+          keyExtractor={(item, index) => index.toString()}
+        />
+      </SafeAreaView>
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 0.9,
-    backgroundColor: '#F5FCFF'
-  },
-  topBar: {
-    height: 56,
-    paddingHorizontal: 16,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center' ,
-    elevation: 6,
-    backgroundColor: '#7B1FA2'
-  },
-  heading: {
-    fontWeight: 'bold',
-    fontSize: 16,
-    alignSelf: 'center',
-    color: '#FFFFFF'
-  },
-  enableInfoWrapper: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center'
-  },
-  tab: {
-    alignItems: 'center',
-    flex: 0.5,
-    height: 56,
-    justifyContent: 'center',
-    borderBottomWidth: 6,
-    borderColor: 'transparent'
-  },
-  connectionInfoWrapper: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    paddingHorizontal: 25
-  },
-  connectionInfo: {
-    fontWeight: 'bold',
-    alignSelf: 'center',
-    fontSize: 18,
-    marginVertical: 10,
-    color: '#238923'
-  },
-  listContainer: {
-    borderColor: '#ccc',
-    borderTopWidth: 0.5
-  },
-  listItem: {
-    flex: 1,
-    height: 48,
-    paddingHorizontal: 16,
-    borderColor: '#ccc',
-    borderBottomWidth: 0.5,
-    justifyContent: 'center'
-  },
-  fixedFooter: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderTopWidth: 1,
-    borderTopColor: '#ddd'
-  },
-  button: {
-    height: 36,
-    margin: 5,
-    paddingHorizontal: 16,
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  buttonText: {
-    color: '#7B1FA2',
-    fontWeight: 'bold',
-    fontSize: 14
-  },
-  buttonRaised: {
-    backgroundColor: '#7B1FA2',
-    borderRadius: 2,
-    elevation: 2
+    flex: 1
   }
-})
-
-export default MoodBluetooth
+});
