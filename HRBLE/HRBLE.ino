e1a10ddbe1c66fae046d974aa2d62ef90e8a817b
Galamon Mac <galamon5@hotmail.com>
2018-11-17 22:14:14 -0600

    Just cleaning some comments on code

--- HRBLE/HRBLE.ino
+++ HRBLE/HRBLE.ino
@@ -1,28 +1,8 @@
+
 /*
-  Optical Heart Rate Detection (PBA Algorithm) using the MAX30105 Breakout
-  By: Nathan Seidle @ SparkFun Electronics
-  Date: October 2nd, 2016
-  https://github.com/sparkfun/MAX30105_Breakout
-
-  This is a demo to show the reading of heart rate or beats per minute (BPM) using
-  a Penpheral Beat Amplitude (PBA) algorithm.
-
-  It is best to attach the sensor to your finger using a rubber band or other tightening
-  device. Humans are generally bad at applying constant pressure to a thing. When you
-  press your finger against the sensor it varies enough to cause the blood in your
-  finger to flow differently which causes the sensor readings to go wonky.
-
-  Hardware Connections (Breakoutboard to Arduino):
-  -5V = 5V (3.3V is allowed)
-  -GND = GND
-  -SDA = A4 (or SDA)
-  -SCL = A5 (or SCL)
-  -INT = Not connected
-
-  The MAX30105 Breakout can handle 5V or 3.3V I2C logic. We recommend powering the board with 5V
-  but it will also run at 3.3V.
+Based on a demo of particleSensor Max30105
+This was made by Gala, part of the team Moodable
 */
-
 #include <Wire.h>
 #include "MAX30105.h"
 #include "heartRate.h"
@@ -36,14 +16,14 @@ void setup()
   moodable.begin(9600);
   moodable.println("Initializing");
   // Initialize sensor
-  if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
+  if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) 
   {
     moodable.println("E01");
     while (1);
   }
   moodable.println("W01");
 
-  particleSensor.setup(); //Configure sensor with default settings
+  particleSensor.setup(); 
   particleSensor.setPulseAmplitudeRed(0x0A); //Turn Red LED to low to indicate sensor is running
   particleSensor.setPulseAmplitudeGreen(0); //Turn off Green LED
 }
@@ -54,7 +34,6 @@ void loop()
 
   if (checkForBeat(irValue) == true)
   {
-    //We sensed a beat!
     moodable.write("Beat");
   }else{
     if (irValue < 50000){
